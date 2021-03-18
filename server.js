4332da43d17afba482fad4fa7d728fcdd6781c48
Julia Windham <julia.windham0@gmail.com>
2021-03-18 12:42:16 -0400

    diff pool config

--- server.js
+++ server.js
@@ -10,29 +10,38 @@ var jsonParser = bodyParser.json()
 
 // postgre
 
-// const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-// pool.on('error', (err, client) => {
-//   console.error('Unexpected error on idle client', err)
-//   process.exit(-1)
-// })
-
-const client = new Client({ 
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false
- })
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
+const dns = require('dns');
+var hostname = os.hostname();
+// const hostname = "jwindha1"
+console.log("hostname", hostname)
+dns.lookup(hostname, e => console.log("host", e))
+dns.lookup(hostname, {hints: dns.ADDRCONFIG|dns.V4MAPPED}, e => console.log("hints", e))
+
+// const client = new Client(c)
 
 async function query(q) {
 
-  // let result = null, client = null
-  // try {
-  //   client = await pool.connect()
-  //   result = await client.query(q)
-  // } catch (err) {
-  //   console.log(err.stack)
-  // } finally {
-  //   if (client !== null) client.release()
-  // }
-  // return result
+  let result = null, client = null
+  try {
+    client = await pool.connect()
+    // result = await client.query(q)
+  } catch (err) {
+    console.log("query error", err.stack)
+  } finally {
+    if (client !== null) client.release()
+  }
+  return result
 
   // return pool
   //   .connect()
@@ -51,16 +60,16 @@ async function query(q) {
   //     return result
   //   })
 
-  client
-  .connect()
-  .then(() => console.log('connected'))
-  .catch(err => console.error('connection error', err.stack))
+  // client
+  // .connect()
+  // .then(() => console.log('connected'))
+  // .catch(err => console.error('connection error', err.stack))
 
-  client
-  .query('SELECT table_schema,table_name FROM information_schema.tables;')
-  .then(result => console.log(result))
-  .catch(e => console.error(e.stack))
-  .then(() => client.end())
+  // client
+  // .query('SELECT table_schema,table_name FROM information_schema.tables;')
+  // .then(result => console.log(result))
+  // .catch(e => console.error(e.stack))
+  // .then(() => client.end())
 }
 
 // apis
@@ -73,7 +82,7 @@ app.post('/add', jsonParser, (req, res) => {
   const table = req.body.table
   const cols = Object.keys(req.body.data).join(", ")
   const vals = Object.values(req.body.data).join(", ")
-  query(`INSERT INTO ${table}(${cols}) VALUES (${vals});`).then(console.log)
+  query(`INSERT INTO ${table}(${cols}) VALUES (${vals});`).then(v => console.log("query result", v))
   res.send(`Added to database: ${req.body}`);
 });
 
