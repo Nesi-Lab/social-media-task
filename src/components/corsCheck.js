5f5f830332fc47e46a2d51d280e29bf998fe498a
Julia Windham <julia.windham0@gmail.com>
2021-03-23 20:14:16 -0400

    query col fix

--- src/components/corsCheck.js
+++ src/components/corsCheck.js
@@ -64,7 +64,7 @@ export default function CorsCheck(props) {
 
     async function save() {
         props.curr.wgLogs.push({ timestamp: Date.now(), id: "end-cors-check", good: good })
-        writeData("metadata", {success: "!"})
+        writeData("metadata", {name: "success", value: "!", participant_id: "dummy", timestamp: Date.now()})
     }
 
     useEffect(() => {
